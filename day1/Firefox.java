cb4ca399fd305bfab3ec396a966d029fa6acea71
Vishnu0491 <139791394+Vishnu0491@users.noreply.github.com>
2023-07-22 10:23:58 +0530

    Add files via upload

---
+++ day1/Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 100.2f;
+		String name = "firefox";
+		boolean visible = true;
+		int year = 1998;
+		char logo = 'm';
+		System.out.println("Browser name " +name);
+		System.out.println("Firefox version " +version);
+		System.out.println("Introduced Year " +year);
+		System.out.println("Logo " +logo);
+		System.out.println("Is the Browser visible " +visible);
+	}
+
+}
